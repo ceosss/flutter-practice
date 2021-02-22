d1361987f872cd2a53a39e687091a2f705d2309d
ceosss <sswarajsamant@gmail.com>
2021-02-23 00:17:54 +0530

    MI-CARD

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,73 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(MiCard());
+}
+
+class MiCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-    backgroundColor: Colors.deepPurple[100],
-    appBar: AppBar(
-      title: Text("I'M POOR"),
-      backgroundColor: Colors.deepPurple[400],
-    ),
-    body: Center(
-      child: Image.asset(
-        "images/coal.png",
-        fit: BoxFit.fitWidth,
+        backgroundColor: Colors.green[100],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    "images/profile.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0, bottom: 5),
+                  child: Text(
+                    "Swaraj",
+                    style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 2,
+                        fontFamily: 'Pacifico'),
+                  ),
+                ),
+                Text("Full Stack Developer",
+                    style: TextStyle(fontFamily: 'Roboto')),
+                SizedBox(
+                  width: 100,
+                  child: Divider(
+                    color: Colors.green[500],
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Text(
+                      "+91 8838887214",
+                      style: TextStyle(fontFamily: 'Roboto'),
+                    ),
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.green[300],
+                    ),
+                  ),
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Text("sswarajsamant@gmail.com",
+                        style: TextStyle(fontFamily: 'Roboto')),
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.green[300],
+                    ),
+                  ),
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
-    ),
-  )));
+    );
+  }
 }
